16e86327de4f9f6fe3d50dd7d82a975a97dbb768
Michael <mikaelplata@gmail.com>
2021-11-15 16:19:49 +0000

    add new transaction

--- controllers/transaction_controller.py
+++ controllers/transaction_controller.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.transaction import Transaction
 import repositories.transaction_repository as transaction_repository
+import repositories.merchant_repository as merchant_repository
+import repositories.label_repository as label_repository
 
 transactions_blueprint = Blueprint("transactions", __name__)
 
@@ -9,4 +11,11 @@ transactions_blueprint = Blueprint("transactions", __name__)
 @transactions_blueprint.route("/transactions")
 def transactions():
     transactions = transaction_repository.select_all()
-    return render_template("transactions/new.html")
+    return render_template("transactions/index.html", transactions=transactions)
+
+@transactions_blueprint.route("/transactions/new")
+def new_transaction():
+    merchants = merchant_repository.select_all()
+    labels = label_repository.select_all()
+    return render_template("transactions/new.html", merchants=merchants, labels=labels)
+
